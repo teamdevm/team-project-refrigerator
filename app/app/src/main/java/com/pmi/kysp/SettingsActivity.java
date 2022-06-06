be05e3754833c8358c294fe2dae76520f3166c86
romesful <ilya.romesful@gmail.com>
2022-06-06 20:16:32 +0500

    Добавлен TimePicker диалог для времени уведомлений

--- app/app/src/main/java/com/pmi/kysp/SettingsActivity.java
+++ app/app/src/main/java/com/pmi/kysp/SettingsActivity.java
@@ -1,12 +1,20 @@
 package com.pmi.kysp;
 
 import android.app.Activity;
+import android.app.TimePickerDialog;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.ImageButton;
+import android.widget.TextView;
+import android.widget.TimePicker;
 
 import androidx.annotation.Nullable;
 
+import java.util.Locale;
+
 public class SettingsActivity extends Activity {
+    int hour, minute;
+    TextView timeText;
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -14,5 +22,31 @@ public class SettingsActivity extends Activity {
 
         ImageButton btnSettings = findViewById(R.id.footer__settings_button);
         btnSettings.setActivated(true);
+
+        // TODO: Запоминание времени + подгрузка при открытии настроек
+        timeText = findViewById(R.id.settings__time_text);
+        timeText.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                popTimePicker(v);
+            }
+        });
+    }
+    public void popTimePicker(View view)
+    {
+        TimePickerDialog.OnTimeSetListener onTimeSetListener = new TimePickerDialog.OnTimeSetListener()
+        {
+            @Override
+            public void onTimeSet(TimePicker timePicker, int selectedHour, int selectedMinute)
+            {
+                hour = selectedHour;
+                minute = selectedMinute;
+                timeText.setText(String.format(Locale.getDefault(), "%02d:%02d", hour, minute));
+            }
+        };
+        TimePickerDialog timePickerDialog = new TimePickerDialog(this, onTimeSetListener, hour, minute, true);
+
+        timePickerDialog.setTitle("Время уведомления");
+        timePickerDialog.show();
     }
 }
