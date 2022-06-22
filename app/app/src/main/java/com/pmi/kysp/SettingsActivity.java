b9870c0e3d4e516ae2afa0f5e7bf47b092499608
Teny777 <shest0100@gmail.com>
2022-06-22 18:17:07 +0500

    Сохранение значений в настройках
    Сохранение значений в настройках при закрытии. Загрузка значений в настройках при открытии.

--- app/app/src/main/java/com/pmi/kysp/SettingsActivity.java
+++ app/app/src/main/java/com/pmi/kysp/SettingsActivity.java
@@ -4,7 +4,9 @@ import android.app.Activity;
 import android.app.TimePickerDialog;
 import android.content.DialogInterface;
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.preference.PreferenceManager;
 import android.view.View;
 import android.widget.ImageButton;
 import android.widget.NumberPicker;
@@ -15,24 +17,35 @@ import android.widget.Toast;
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AlertDialog;
 
+import com.google.android.material.switchmaterial.SwitchMaterial;
+
 import java.util.Locale;
 
 public class SettingsActivity extends Activity {
     private static final String[] DAYS = {"1 день", "2 дня", "3 дня", "4 дня", "5 дней", "6 дней", "7 дней", "8 дней", "9 дней", "10 дней"};
     int hour, minute;
     TextView timeText;
+    SwitchMaterial switchMaterial;
+    NumberPicker numberPicker;
     Toast toast, toastError;
+    SharedPreferences sharedPreferences;
+    final static String SWITCH_NOTIFICATIONS = "switch_notifications";
+    final static String TIME_TEXT = "time_text";
+    final static String NUMBER_PICKER_DAYS = "number_picker_days";
+
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_settings);
 
-        NumberPicker numberPicker = findViewById(R.id.np);
+        numberPicker = findViewById(R.id.np);
         numberPicker.setMinValue(1);
         numberPicker.setMaxValue(10);
         numberPicker.setDisplayedValues(DAYS);
 
-        // TODO: Запоминание времени + подгрузка при открытии настроек
+        switchMaterial = findViewById(R.id.settings__switch_notifications);
+
+
         timeText = findViewById(R.id.settings__time_text);
         timeText.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -105,5 +118,40 @@ public class SettingsActivity extends Activity {
             toast = Toast.makeText(getApplicationContext(), "Не удалось считать штрих-код", Toast.LENGTH_LONG);
             toast.show();
         }
+    }
+    @Override
+    protected void onStart(){
+        super.onStart();
+        loadPreferences();
+    }
+
+    @Override
+    protected void onStop(){
+        super.onStop();
+        savePreferences();
+    }
+
+
+    private void savePreferences(){
+        sharedPreferences = getSharedPreferences("SettingsNotification", MODE_PRIVATE);
+        SharedPreferences.Editor ed = sharedPreferences.edit();
+        ed.putBoolean(SWITCH_NOTIFICATIONS, switchMaterial.isChecked());
+        ed.putString(TIME_TEXT, timeText.getText().toString());
+        ed.putInt(NUMBER_PICKER_DAYS, numberPicker.getValue());
+        ed.commit();
+    }
+
+    private void loadPreferences(){
+        sharedPreferences = getSharedPreferences("SettingsNotification", MODE_PRIVATE);
+        boolean switchNotificationsValue = sharedPreferences.getBoolean(SWITCH_NOTIFICATIONS, true);
+        String timeTextValue = sharedPreferences.getString(TIME_TEXT, "17:00");
+        int npDaysValue = sharedPreferences.getInt(NUMBER_PICKER_DAYS, 1);
+        setPreferences(switchNotificationsValue, timeTextValue, npDaysValue);
+    }
+
+    private void setPreferences(boolean switchNotifications, String timeTextValue, int npDaysValue){
+        switchMaterial.setChecked(switchNotifications);
+        timeText.setText(timeTextValue);
+        numberPicker.setValue(npDaysValue);
     }
 }
