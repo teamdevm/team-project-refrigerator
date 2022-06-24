538157a89a9c7f7ce53372711a74ca3d8595356d
Zakharov Ilya <ilya.romesful@gmail.com>
2022-06-24 14:01:08 +0500

    Добавлен ID для категории

--- app/app/src/main/java/com/pmi/kysp/Category.java
+++ app/app/src/main/java/com/pmi/kysp/Category.java
@@ -1,13 +1,20 @@
 package com.pmi.kysp;
 
 public class Category {
+    private int id;
     private String name;
     public boolean isChosen;
 
-    public Category(String name, boolean isChosen)
+    public Category(int id, String name, boolean isChosen)
     {
+        this.id = id;
         this.name = name;
         this.isChosen = isChosen;
+    }
+
+    public Category(int id, String name)
+    {
+        this(id, name, false);
     }
 
     public String getName()
